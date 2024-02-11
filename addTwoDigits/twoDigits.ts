149deb79ff775f9b4a481f8bf49569428857ccce
listenToRipley <natalie.m.kendrick@gmail.com>
2024-02-11 16:29:24 -0500

    same but different

--- addTwoDigits/twoDigits.ts
+++ addTwoDigits/twoDigits.ts
@@ -1,12 +1,17 @@
 export const twoDigits = (num: number): number => {
-  let total = 0;
+  // let total = 0;
 
-  let numArray: string[] = num.toString().split(''); // now should be an array of numbers.
-  numArray.forEach(n => {
-    total+=parseInt(n);
-  });
+  let nums: string[] = num.toString().split(''); // now should be an array of numbers.
+  // nums.forEach(n => {
+  //   total+=parseInt(n);
+  // });
+  // return total;
+  // OOOOORRRRR 
 
-  return total;
+  return nums.reduce((a:string, b:string) => {
+    return parseInt(a) + parseInt(b);
+  }); 
+
 };
 
 //console.log(twoDigits(29)); //expecting 11
