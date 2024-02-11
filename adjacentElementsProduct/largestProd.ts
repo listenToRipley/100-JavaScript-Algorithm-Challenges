08fdb7e7278bfa5c0aadb885dcc1f5ecf5b9965d
listenToRipley <natalie.m.kendrick@gmail.com>
2024-02-11 17:01:18 -0500

    end before we go beyond the array

--- adjacentElementsProduct/largestProd.ts
+++ adjacentElementsProduct/largestProd.ts
@@ -3,7 +3,7 @@ export const largestProd = (products:number[]): number[] => {
   let multiple: number = products[0] * products[1];//starting comparing values will be the first two elements multiplied.
 
   products.forEach((num: number, idx: number) => {
-    if (num * products[idx +1] > multiple) {
+    if (idx + 1 < products.length && num * products[idx +1] > multiple) {
       largestSet = [num, products[idx +1]];
       multiple = num * products[idx +1];
     }
