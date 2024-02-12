36d7dd1cade7a2411d88c8595eeb75de06589c42
listenToRipley <natalie.m.kendrick@gmail.com>
2024-02-12 18:31:42 -0500

    refine terms

--- allLongestStrings/longestStrings.ts
+++ allLongestStrings/longestStrings.ts
@@ -2,12 +2,12 @@ export const longestStrings = (list: string[]): string[] => {
   let longest = list[0].length //starting value
   let result = [];
 
-  list.forEach((str)=> {
-    if (str.length  == longest) {
-      result.push(str);
-    } else if (str.length > longest) {
-      longest = str.length //now the longest is this
-      result = [str]// reset  the results array.
+  list.forEach((word:string)=> {
+    if (word.length  == longest) {
+      result.push(word);
+    } else if (word.length > longest) {
+      longest = word.length //now the longest is this
+      result = [word]// reset  the results array.
     }//if the current string is shorter than longest, then we should just ignore it.
 
   });
