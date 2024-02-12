280e0f0c84fb0aedbca2a6f5ca52e8733f921517
listenToRipley <natalie.m.kendrick@gmail.com>
2024-02-12 18:20:05 -0500

    first pass on longest

--- allLongestStrings/longestStrings.ts
+++ allLongestStrings/longestStrings.ts
@@ -1,1 +1,17 @@
-export const longestStrings 
+export const longestStrings = (list: string[]): string[] => {
+  let longest = list[0].length //string value
+  let result = [];
+
+  list.forEach((str)=> {
+    console.log(str)
+    if (str.length  == longest) {
+      result.push(str);
+    } else if (str.length > longest) {
+      longest = str.length
+      result = [str]
+    }//if the current string is shorter than longest, then we should just ignore it.
+
+  });
+
+  return result;
+};
