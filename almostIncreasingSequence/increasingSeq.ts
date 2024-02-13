ba1a9e55700bf029f79d7d11385abb4146ed6079
listenToRipley <natalie.m.kendrick@gmail.com>
2024-02-12 19:05:04 -0500

    passes for option two

--- almostIncreasingSequence/increasingSeq.ts
+++ almostIncreasingSequence/increasingSeq.ts
@@ -4,31 +4,31 @@ export const increasingSeq = (seq: number[]):boolean => {
   let unsortedSeq = [...seq] // so we are always retaining the original 
 
   //option 1
-  let sortedSeq: number[] = [...seq].sort((a:number,b:number) => a-b); 
+  // let sortedSeq: number[] = [...seq].sort((a:number,b:number) => a-b); 
 
-  for (let i = 0; i < unsortedSeq.length - 1; i++) {
-    if (unsortedSeq[i] != sortedSeq[i] && removed == 0) {
-      delete unsortedSeq[i];
-      delete sortedSeq[i];
-      console.log(`unsorted now: ${unsortedSeq}\n sorted now: ${sortedSeq}`);
-      removed += 1;
-      console.log(`removed? ${removed}`);
-    } else {
-      return increasing = false;
-    };
-  };
-
-  //option 2
-  // for (let j = 0; j < unsortedSeq.length - 1; j++) {
-  //   if ( unsortedSeq[j + 1] > unsortedSeq[j] && removed == 0) {
-  //     unsortedSeq.splice(j + 1, 1);//instead of delete 
-  //     console.log(`unsorted now: ${unsortedSeq}`);
+  // for (let i = 0; i < unsortedSeq.length - 1; i++) {
+  //   if (unsortedSeq[i] != sortedSeq[i] && removed == 0) {
+  //     delete unsortedSeq[i];
+  //     delete sortedSeq[i];
+  //     console.log(`unsorted now: ${unsortedSeq}\n sorted now: ${sortedSeq}`);
   //     removed += 1;
   //     console.log(`removed? ${removed}`);
   //   } else {
   //     return increasing = false;
-  //   };  
+  //   };
   // };
+
+  //option 2
+  for (let j = 0; j < unsortedSeq.length - 1; j++) {
+    if ( unsortedSeq[j + 1] > unsortedSeq[j] && removed == 0) {
+      unsortedSeq.splice(j + 1, 1);//instead of delete 
+      console.log(`unsorted now: ${unsortedSeq}`);
+      removed += 1;
+      console.log(`removed? ${removed}`);
+    } else {
+      return increasing = false;
+    };  
+  };
 
   return increasing;
 }
