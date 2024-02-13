b1a78d7fd9b88cc67ec3a90429cad5d675e73442
listenToRipley <natalie.m.kendrick@gmail.com>
2024-02-13 18:51:51 -0500

    first pass

---
+++ alphabeticShift/alphabetShift.ts
@@ -0,0 +1,12 @@
+export const alphabetShift = (word:string): string => {
+  let alphabet: string[] = 'abcdefghijklmnopqrstuvwxyz'.split('');//alphabet array
+  let wordArray: string[] = word.split('');
+  let newWordArray: string[] = []
+  
+  wordArray.forEach((letter: string) => {
+    let index = alphabet.indexOf(letter)
+    index === alphabet.length ? newWordArray.push(alphabet[0]) : newWordArray.push(alphabet[index + 1]);
+  })
+
+  return newWordArray.join('');
+}
