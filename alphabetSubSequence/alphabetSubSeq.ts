43d63dabf75bc96d398d4a696d38552dc83c45ba
listenToRipley <natalie.m.kendrick@gmail.com>
2024-02-13 22:01:07 -0500

    passing all test but one. I think I am missing something about the origianl question

--- alphabetSubSequence/alphabetSubSeq.ts
+++ alphabetSubSequence/alphabetSubSeq.ts
@@ -5,9 +5,12 @@ export const alphabetSubSeq = (seq: string): boolean => {
   let latestIdx = alphabet.indexOf(seqArray[0]);//have to bet this
 
   for (let i = 1; i < seqArray.length - 1; i++) { //start at the second item since we have already set the next value.
-    let letterIdx = alphabet.indexOf(seqArray[i])
-    if (latestIdx < letterIdx) {
+    let letterIdx = alphabet.indexOf(seqArray[i]);
+    console.log(`indexes: ${latestIdx} < ${letterIdx}`)
+    if (latestIdx > letterIdx) {
       return inOrder = false;
+    } else {
+      latestIdx = alphabet.indexOf(seqArray[i])
     }
 
   }
