4e13beb8839bcfbd86de942c2b28dafec3240bde
listenToRipley <natalie.m.kendrick@gmail.com>
2024-02-24 10:59:57 -0500

    reworked for misunderstanding of original issue

--- areSimilar/areSimilar.ts
+++ areSimilar/areSimilar.ts
@@ -1,31 +1,29 @@
 export const areSimilar = (group1: number[], group2: number[]):boolean => {
-  let similar = false;
+  let similar: boolean = false;
   //short circuit if one is longer than the other.
   if (group1.length !== group2.length) {
     return similar;
   }
   //track how many times we have to move a number.
-  let swapCount1: number = 0;
-  let swapCount2:number = 0;
+  let swapCount: number = 0;
+  let newGroup2: number[] = []
   
-  let string1: string = [...group1].sort((a:number,b:number):any => {
-    if (a-b > 0) {
-      swapCount1 +=1;
+  group1.forEach((value: number, idx: number ) => {
+    if (value != group2[idx]) {
+      let findValue: number = group2.indexOf(value);
+      if (findValue == -1) {
+        return similar = false;
+      } else {
+        newGroup2.push(value);
+        swapCount ++;
+      }
+    } else {
+      newGroup2.push(group2[idx])
     }
-    return a-b
-  }).join('');
-  let string2: string = [...group2].sort((a:number,b:number):any => {
-    if (a-b > 0) {
-      swapCount2 += 1;
-    }
-    return a-b
-  }).join('');
-
-  const swapDiff = Math.abs(swapCount1 - swapCount2);//check to see if you have to reorder more on one than the other. If there different is greater than 2, then we had to move too many times.
-
-  if (swapDiff <= 1 && string1 === string2) {
-    similar = true;
-  }
+  });
+  if (group1.toString() === newGroup2.toString() && swapCount <= 1) {
+    return similar = true;
+  } 
 
   return similar;
 };
