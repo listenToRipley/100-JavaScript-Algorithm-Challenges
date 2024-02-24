bbbdadfa7fe882aabc27a68be73774cb3141bc15
listenToRipley <natalie.m.kendrick@gmail.com>
2024-02-24 09:49:07 -0500

    first pass on solution

---
+++ areSimilar/areSimilar.ts
@@ -0,0 +1,10 @@
+export const areSimilar = (group1: number[], group2: number[]):boolean => {
+  let string1: string = [...group1].sort((a:number,b:number)=> a-b).join('');
+  let string2: string = [...group2].sort((a:number,b:number)=> a-b).join('');
+
+  return string1 === string2 ? true : false;
+}
+
+console.log(areSimilar([1,2,3], [1,2,3]))//true
+console.log(areSimilar([1,2,3], [2,1,3]))//true
+console.log(areSimilar([1,2,2,], [1,1,2]))//false
