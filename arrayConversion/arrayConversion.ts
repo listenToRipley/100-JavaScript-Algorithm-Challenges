05ada6a1320e0d97a328ea080db777c26502bbc0
listenToRipley <natalie.m.kendrick@gmail.com>
2024-03-14 20:42:03 -0400

    first pass

---
+++ arrayConversion/arrayConversion.ts
@@ -0,0 +1,45 @@
+export const arrayConversion = (sumArr: number[]):number => {
+  let result: number = 0;
+  let iterationCount = 0;
+
+  let currentArray: number[][]| number[] = loopPairs(sumArr)
+
+  do { //if the result has not yet been returned, then we need to break down the array still.
+
+    if (iterationCount % 2 === 0) {
+      iterationCount++;
+      AddLoop(currentArray);
+    } 
+
+    if (iterationCount % 2 !== 0) {
+      iterationCount++;
+      mutlipleLoop(currentArray)
+    }
+  } while (currentArray.length !== 1) // break condition
+
+  return currentArray[0];
+};
+
+const loopPairs = (largeArr: number[]): number[][] => {
+  let newArray: number[][]|undefined = []
+  for (let i = 0; i < largeArr.length-1; i++) {
+    newArray.push(largeArr.slice(i, i+2))
+  };
+  return newArray
+};
+
+const mutlipleLoop = (multipleArr: number[][]): number[] => {
+  let result: number[] = []
+  for (let i = 0; i < multipleArr.length - 1; i++) {
+    result.push(multipleArr[i][0] * multipleArr[i][1])
+  }
+  return result;
+};
+
+const AddLoop = (addArr: number[][]): number[] => {
+  let result: number[] = []
+  for (let i = 0; i < addArr.length - 1; i++) {
+    result.push(addArr[i][0] + addArr[i][1])
+  }
+  return result;
+};
