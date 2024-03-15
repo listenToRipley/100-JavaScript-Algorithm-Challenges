89ff1f5150103c1c519411d4a9ee64e50ae0cd95
listenToRipley <natalie.m.kendrick@gmail.com>
2024-03-14 20:45:09 -0400

    add testing

---
+++ arrayConversion/arrayConversion.spec.ts
@@ -0,0 +1,10 @@
+import { arrayConversion } from './arrayConversion';
+
+describe(arrayConversion.name, () => {
+  it('test1: array conversion', () => {
+    //function call
+    const result = arrayConversion([1,2,3,4,5,6,7,8]);
+    //assert
+    expect(result).toBe(186)
+  })
+})
