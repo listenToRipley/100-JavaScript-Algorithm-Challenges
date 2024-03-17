b85818d2571a2c23facb56510534970fa0ae3981
listenToRipley <natalie.m.kendrick@gmail.com>
2024-03-17 14:32:31 -0400

    resolved syntax and make simplier, array conversion passing all tests

--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -1,45 +1,23 @@
 export const arrayConversion = (sumArr: number[]):number => {
   let isOdd: boolean = true;
 
-  let currentArray: number[][]| number[] = sumArr.length > 1 ? loopPairs(sumArr) : sumArr;
-  while(currentArray.length !== 1) { //if the result has not yet been returned, then we need to break down the array still.
-    isOdd = !isOdd
-    if (isOdd) {
-      console.log('is odd')
-      arrayConversion(mutlipleLoop(currentArray));
-    } else {
-      arrayConversion(AddLoop(currentArray)); 
-    }
-  } 
-
-  return currentArray[0];
-};
-
-const loopPairs = (largeArr: number[][]): number[] | number[][] => {
-  let newArray: number[][]|undefined = []
-  for (let i = 0; i < largeArr.length-1; i++) {
-    newArray.push([largeArr.slice(i, i+2)])
+  while (sumArr.length !== 1) {
+    sumArr = sumProduct(sumArr, isOdd);
+    isOdd = !isOdd;
   };
-  console.log('pairing : ', newArray)
-  return newArray
+  return sumArr[0]
 };
 
-const mutlipleLoop = (multipleArr: number[][]): number[] => {
-  let result: number[] = []
-  for (let i = 0; i < multipleArr.length - 1; i++) {
-    result.push(multipleArr[i][0] * multipleArr[i][1])
+const sumProduct = (nums: number[], isOdd: boolean): number[] => {
+  const sumProducts: number[] = [];
+
+  for (let i = 0; i < nums.length; i+=2) { //increase by 2
+    if (isOdd) {
+      sumProducts.push(nums[i] + nums[i+1]);
+    } else {
+      sumProducts.push(nums[i] * nums[i+1]);
+    }
   }
-  console.log('multiply results: ', result)
-  return result;
+  return sumProducts;
 };
-
-const AddLoop = (addArr: number[][]): number[] => {
-  let result: number[] = []
-  for (let i = 0; i < addArr.length - 1; i++) {
-    result.push(addArr[i][0] + addArr[i][1])
-  }
-  console.log('add result: ', result)
-  return result;
-};
-
 console.log(arrayConversion([1,2,3,4,5,6,7,8]))
