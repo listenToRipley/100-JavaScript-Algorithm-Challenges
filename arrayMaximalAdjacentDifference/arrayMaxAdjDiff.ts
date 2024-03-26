929076ac7581c9ea2d9c64b893aa3a496d8147e0
listenToRipley <natalie.m.kendrick@gmail.com>
2024-03-26 19:51:32 -0400

    small adjustment to start on second value

--- arrayMaximalAdjacentDifference/arrayMaxAdjDiff.ts
+++ arrayMaximalAdjacentDifference/arrayMaxAdjDiff.ts
@@ -1,7 +1,7 @@
 export const arrayMaxAdjDiff = (inputArray: number[]):number => {
-  let max: number = 0;
+  let max: number = Math.abs(inputArray[0] - inputArray[1]);
 
-  for (let i = 0; i < inputArray.length-1; i++) {
+  for (let i = 1; i < inputArray.length-1; i++) {
     if (inputArray[i+1]) { //valid there is a next value
       let abs:number = Math.abs(inputArray[i] - inputArray[i+1])
       max = abs > max ? abs : max;
