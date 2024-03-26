d49aae0711f76c748e158f05f18c88f75f6d8f67
listenToRipley <natalie.m.kendrick@gmail.com>
2024-03-26 19:50:22 -0400

    passing tests

--- arrayMaximalAdjacentDifference/arrayMaxAdjDiff.spec.ts
+++ arrayMaximalAdjacentDifference/arrayMaxAdjDiff.spec.ts
@@ -8,4 +8,11 @@ describe(arrayMaxAdjDiff.name, () => {
     //assert
     expect(result).toBe(3);
   });
+
+  it('test2: max adj diff', ()=> {
+    //function call
+    const result = arrayMaxAdjDiff([2, 9, 1, 0])
+    //assert
+    expect(result).toBe(8)
+  });
 });
