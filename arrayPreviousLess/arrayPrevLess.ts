4a930d10a9774760e60ef1822a5762305997896d
listenToRipley <natalie.m.kendrick@gmail.com>
2024-03-26 20:35:02 -0400

    small adjustment, passing all tests

--- arrayPreviousLess/arrayPrevLess.ts
+++ arrayPreviousLess/arrayPrevLess.ts
@@ -1,8 +1,9 @@
 export const arrayPrevLess = (inputArray: number[]): number[] => {
-  for (let i = inputArray.length; i > 0; i--) {
-    inputArray[i] = inputArray[i] <= inputArray[i+1] ?inputArray[i+1] : -1
-  };
-  return inputArray;
+  let copyInputArray: number[] = [...inputArray];
+  for (let i = copyInputArray.length-1; i >=0; i--) {
+    copyInputArray[i] = copyInputArray[i] >= copyInputArray[i-1] ? copyInputArray[i-1] : -1
+  }
+  return copyInputArray;
 };
 
 console.log(arrayPrevLess([3, 5, 2, 4, 5]));
